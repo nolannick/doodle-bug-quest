49cf477819f7262ad7c3466a2803ff47cb0a5948
Queen <youngidia@live.com>
2019-03-28 13:29:31 -0400

    page layout

--- client/src/components/registrationForm.js
+++ client/src/components/registrationForm.js
@@ -24,7 +24,7 @@ const RegistrationForm = (props) => (
         </div>
         <br/>
         <p><button className="loginButton" onClick={props.register}>Register</button></p>
-        <p>Already Registered? Click <a href="/#" onClick={props.toggleLogin}>here</a> to login instead.</p><br/><br/><br/><br/><br/>
+        <p>Already Registered? Click <a href="/#" onClick={props.toggleLogin}>here</a> to login instead.</p><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
     </form>
 );
