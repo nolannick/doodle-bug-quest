10c4d3892256810b9b0a8f4b65b4e8488b25621d
nolannick <nolannick@gmail.com>
2019-03-29 12:03:46 -0400

    updates to styles on fam page

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './restyles.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
