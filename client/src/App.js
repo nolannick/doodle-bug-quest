228ce77f98b76677b2e6473343ce0e8e030e08b9
Queen <youngidia@live.com>
2019-03-29 11:07:11 -0400

    save 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/header"
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from './components/login';
 import Family from './components/family';
 import Quest from './components/quest';
 import Reward from './components/reward';
+import ErrorPage from './components/errorPage';
 
 class App extends Component {
   render() {
@@ -13,10 +14,14 @@ class App extends Component {
       <BrowserRouter>
         <div >
           <Header />
+          <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/family" component={Family} />
           <Route exact path="/quest" component={Quest} />
           <Route exact path="/reward" component={Reward} />
+          <Route component={ ErrorPage}/>
+          </Switch>
+
         </div>
       </BrowserRouter>
 
